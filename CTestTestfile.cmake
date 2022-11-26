4aae4be1362b4e5a72b6ebfa04159071ab3307b7
lils-code <message-lil@proton.me>
2022-11-26 00:53:04 -0500

    0.1

---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lil/git/marching/shader-sdf-generator
+# Build directory: /home/lil/git/marching/shader-sdf-generator
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/CMakeFiles/sdf-generator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdf-generator.dir/src/main.cpp.o"
+  "sdf-generator"
+  "sdf-generator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sdf-generator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
